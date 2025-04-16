b4f75f901524edee33c56e875a350af2ecb7a8fc
leven1213 <104328146@student.swin.edu.au>
2025-04-16 16:25:44 +1000

    Revise prisma.ts file

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,13 +1,13 @@
-import { neonConfig } from '@neondatabase/serverless';
-import { PrismaNeon } from '@prisma/adapter-neon';
 import { PrismaClient } from '@prisma/client';
+import { PrismaNeon } from '@prisma/adapter-neon';
+import { Pool, neonConfig } from '@neondatabase/serverless';
 import ws from 'ws';
 
 neonConfig.webSocketConstructor = ws;
 
-const connectionString = process.env.DATABASE_URL!;
- 
-const adapter = new PrismaNeon({ connectionString });
+const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
+
+const adapter = new PrismaNeon(pool);
 
 export const prisma = new PrismaClient({ adapter }).$extends({
   result: {
