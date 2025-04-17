4413e637ab3f1ce3775b48d2797bb74fa9603a04
leven1213 <104328146@student.swin.edu.au>
2025-04-17 14:11:42 +1000

    Update checkout-steps.tsx

--- components/shared/checkout-steps.tsx
+++ components/shared/checkout-steps.tsx
@@ -3,7 +3,7 @@ import { cn } from "@/lib/utils";
 
 const CheckoutSteps = ({ current = 0 }) => {
   return (
-    <div className="flex-between flex-col space-x-2 space-y-2 mb-10">
+    <div className="flex-between flex-row space-x-2 space-y-2 mb-10">
       {["User Login", "Shipping Address", "Payment Method", "Place Order"].map(
         (step, index) => (
           <React.Fragment key={step}>
